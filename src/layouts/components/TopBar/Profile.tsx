a2be4e2e55b57ee5a4557de438656329310c26fd
Coyu97 <106992@student.drenthecollege.nl>
2022-11-28 23:45:47 +0100

    create profile structure

--- src/layouts/components/TopBar/Profile.tsx
+++ src/layouts/components/TopBar/Profile.tsx
@@ -1,3 +1,11 @@
 export const Profile = () => {
-  return <div>profile...</div>;
+  return (
+    <div className="flex">
+      <div className="bg-darkTransparentHighlight w-full rounded-2xl  ">
+        <div className="">Background Image...</div>
+        <div className="">Profile Image...</div>
+        <div className="">Profile name/tag/bio ...</div>
+      </div>
+    </div>
+  );
 };
