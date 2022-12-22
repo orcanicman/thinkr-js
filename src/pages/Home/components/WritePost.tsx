850ec26782d3dafc3327575fa46974c7b559cb5e
Alex de Roos <100049383+Alex-de-Roos@users.noreply.github.com>
2022-12-22 22:21:47 +0100

    WritePost early layout design start

--- src/pages/Home/components/WritePost.tsx
+++ src/pages/Home/components/WritePost.tsx
@@ -3,5 +3,26 @@ import React from "react";
 interface WritePostProps {}
 
 export const WritePost: React.FC<WritePostProps> = () => {
-  return <div>writePost</div>;
+  //ideas -> takes in user to show user profile picture and maybe name
+  // -> to autogrow textarea need js on change ig could use it for a character counter in bottom left!
+
+  return (
+  <div className="bg-darkHighlight rounded-xl flex flex-col">
+    <div className="flex flex-row">
+      <div className="bg-gray w-12 h-12 m-2 rounded-full">
+      </div>
+
+      <textarea 
+      className="m-4 bg-darkHighlight outline-none resize-none"
+      placeholder="Whats going on?"
+      minLength={1}
+      maxLength={200}>
+      </textarea>
+    </div>
+    
+    <div className="flex justify-end">
+      <button className="bg-green rounded-2xl py-1 px-4 m-2">Post</button>
+    </div>
+  </div>
+  );
 };
