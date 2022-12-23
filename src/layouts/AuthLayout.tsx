8f7322186adc55e40837cd128aa233e3f6a384bb
BuildTools <unconfigured@null.spigotmc.org>
2022-12-24 00:26:40 +0100

    formatted

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,43 +1,32 @@
 import React from "react";
 import Logo from "../assets/logo";
 
-
 type AuthLayoutProps = {
-    title: string;
-    subTitle: string;
+  title: string;
+  subTitle: string;
 };
 
-
 export const AuthLayout = ({
-    title,
-    subTitle,
-    children,
+  title,
+  subTitle,
+  children,
 }: React.PropsWithChildren<AuthLayoutProps>) => {
-    return (
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="flex justify-center pt-6 pb-4">
+        <Logo />
+      </div>
 
-        <>
+      <div className="text-center text-darkHighlight ">
+        <h2>Thinkr</h2>
+        <h3>"Speak your mind"</h3>
+        <h3>{subTitle}</h3>
+        <h1>{title}</h1>
 
-            <div className=" flex justify-center pt-6 pb-4">
-                <Logo />
-            </div>
+        <div className="border-b border-b-gray items-center">{children}</div>
 
-            <div className=" text-center text-darkHighlight ">
-
-                <h2>Thinkr</h2>
-                <h3>"Speak your mind"</h3>
-                <h3>{subTitle}</h3>
-                <h1>{title}</h1>
-
-                <div className=" border-b border-b-gray items-center">
-                    {children}
-                </div>
-
-                < button > Darkmode</button >
-            </div>
-
-
-
-        </>
-
-    );
+        <button>Darkmode</button>
+      </div>
+    </div>
+  );
 };
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,12 +4,16 @@ import { AuthLayout } from "../../layouts/AuthLayout";
 interface LoginProps {}
 
 export const Login: React.FC<LoginProps> = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    console.log(event);
+  };
+
   return (
     <AuthLayout
       title="Login"
       subTitle="Sing in to see what your friends are thinking!"
     >
-      <form>
+      <form onSubmit={handleSubmit}>
         <input placeholder="your name" />
         <input placeholder="your password" />
         <input />
