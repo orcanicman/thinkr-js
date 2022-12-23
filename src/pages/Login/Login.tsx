6886467c736059b1cbfb337faa9289975694f8f3
BuildTools <unconfigured@null.spigotmc.org>
2022-12-24 00:20:57 +0100

    fixed error

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,21 +1,19 @@
 import React from "react";
-import MainLayout from "../../layouts/Main";
-import { NotLoggedInLayout } from "../../layouts/NotLoggedIn";
+import { AuthLayout } from "../../layouts/AuthLayout";
 
-interface LoginProps { }
+interface LoginProps {}
 
 export const Login: React.FC<LoginProps> = () => {
-    return (
-
-        <AuthLayout
-            title="Login"
-            subTitle="Sing in to see what your friends are thinking!"
-        >
-            <form>
-                <input placeholder="your name" />
-                <input placeholder="your password" />
-                <input />
-            </form>
-        </AuthLayout>
-    );
+  return (
+    <AuthLayout
+      title="Login"
+      subTitle="Sing in to see what your friends are thinking!"
+    >
+      <form>
+        <input placeholder="your name" />
+        <input placeholder="your password" />
+        <input />
+      </form>
+    </AuthLayout>
+  );
 };
