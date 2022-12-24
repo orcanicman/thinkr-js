4291cba186358cd5e88924a1c70f43e544a421b9
Alex de Roos <100049383+Alex-de-Roos@users.noreply.github.com>
2022-12-24 19:29:06 +0100

    big fixes

--- src/pages/Home/components/WritePost.tsx
+++ src/pages/Home/components/WritePost.tsx
@@ -1,61 +1,67 @@
 import React, { useState } from "react";
 import ProfileImage from "../../../assets/Profile_Image.png";
+import { User } from "../../../types/User";
 
-interface WritePostProps {}
+interface WritePostProps {
+	user: User;
+}
 
-export const WritePost: React.FC<WritePostProps> = ({}) => {
-  //ideas -> takes in user to show user profile picture and maybe name
-  // -> to autogrow textarea need js on change ig could use it for a character counter in bottom left!
-  const [characterCount, setCharacterCount] = useState(0);
-  const maxCharCount: number = 200;
+export const WritePost: React.FC<WritePostProps> = ({ user }) => {
+	//ideas -> takes in user to show user profile picture and maybe name
+	// -> to autogrow textarea need js on change ig could use it for a character counter in bottom left!
+	const [value, setValue] = useState("");
+	const maxCharCount: number = 200;
 
-  function inputFieldChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    setCharacterCount(event.target.value.length);
-    console.log(event.target.scrollHeight, event.target.clientHeight);
-    event.target.style.height = "";
-    event.target.style.height = event.target.scrollHeight + "px";
-  }
+	const inputFieldChange = (event: React.FormEvent<HTMLDivElement>) => {
+		setValue(event.currentTarget.innerText);
+	};
 
-  function post() {
-    const value = (document.getElementById("newPostInputField") as HTMLTextAreaElement).value;
-    console.log(value);
-  }
+	const post = () => {
+		console.log(value);
+	};
 
-  return (
-  <div className="bg-darkTransparentHighlight rounded-xl flex flex-col mb-8 p-6">
+	return (
+		<div className="bg-darkTransparentHighlight rounded-xl flex flex-col mb-8 p-6">
+			<div className="flex flex-row">
+				<div className="w-16 h-16 rounded-full mr-6 min-w-[4rem]">
+					<img src={ProfileImage} alt="Poster profile" />
+					{/* <img src={user.profile.picture} alt="Poster profile" /> */}
+				</div>
 
-    <div className="flex flex-row">
-      <div className="w-16 h-16 rounded-full mr-6 min-w-[4rem]">
-        <img src={ProfileImage} alt="Poster profile" />
-      </div>
+				<div className="relative w-full">
+					<div
+						className={`absolute top-0 left-0 text-gray p-1 ${
+							value.length ? "opacity-0" : "opacity-100"
+						}`}
+					>
+						What are you thinking about?
+					</div>
+					<div
+						id="newPostInputField"
+						className="bg-darkTransparentHighlight outline-none resize-none mb-8 rounded-md p-1 w-full whitespace-pre-wrap break-all relative"
+						placeholder="What are you thinking about?"
+						contentEditable
+						onInput={inputFieldChange}
+					/>
+				</div>
+			</div>
 
-      <textarea 
-      id="newPostInputField"
-      className="bg-darkTransparentHighlight outline-none resize-none mb-8 rounded-md p-1 w-full"
-      placeholder="What are you thinking about?"
-      maxLength={maxCharCount}
-      rows={1}
-      onChange={(event) => {
-        inputFieldChange(event);
-      }}
-      >
-      </textarea>
-    </div>
-    
-    <div className="flex justify-between items-end">
-      <div className="text-gray">{ characterCount + " / " + maxCharCount + " characters"}</div>
+			<div className="flex justify-between items-end">
+				<div className="text-gray">
+					<span className={value.length > 200 ? "text-red" : ""}>
+						{value.length}
+					</span>
+					{` / ${maxCharCount} characters`}
+				</div>
 
-      <button
-      className="bg-green rounded-full py-2 px-4 mx-2 transition-all hover:bg-opacity-80 disabled:hover:bg-gray"
-      disabled={characterCount === 0}
-      onClick={(event) => {
-        post();
-      }}
-      >
-        Post
-      </button>
-    </div>
-
-  </div>
-  );
+				<button
+					className="bg-green rounded-full py-2 px-4 mx-2 transition-all hover:bg-opacity-80 disabled:opacity-50 disabled:cursor-not-allowed"
+					disabled={!value.length || value.length > maxCharCount}
+					onClick={post}
+				>
+					Post
+				</button>
+			</div>
+		</div>
+	);
 };
