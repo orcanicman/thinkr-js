5d55bf68d2e189cc54c8cbb199cf9d362b3ff107
Alex <alex@beeholder.nl>
2023-01-03 16:58:33 +0100

    made page component and added routing

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,32 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
+import { ProfilePage } from "./pages/Profile/ProfilePage";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/:tag",
+		element: <ProfilePage />,
+	},
+	{
+		path: "/:tag/followers",
+		element: <ProfilePage />,
+	},
+	{
+		path: "/:tag/following",
+		element: <ProfilePage />,
+	},
 ]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
---
+++ src/pages/Profile/ProfilePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainLayout from "../../layouts/Main";
+
+interface ProfilePageProps {}
+
+export const ProfilePage: React.FC<ProfilePageProps> = ({}) => {
+	return (
+		<MainLayout>
+			<div>lololololololol</div>
+		</MainLayout>
+	);
+};
