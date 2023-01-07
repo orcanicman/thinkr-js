97614e98b36488f86f7185d36ccc6ecb3653745a
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 22:52:02 +0100

    incrementally adopting next

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
