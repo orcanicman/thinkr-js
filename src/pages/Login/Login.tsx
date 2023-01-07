3e6cb0ff994fe8addf342b710d3c9607953efa5c
Coyu97 <106992@student.drenthecollege.nl>
2023-01-07 17:30:30 +0100

    added stykubg

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,14 +5,18 @@ interface LoginProps {}
 
 export const Login: React.FC<LoginProps> = () => {
   return (
-    <AuthLayout
-      title="Login"
-      subTitle="Sing in to see what your friends are thinking!"
-    >
+    <AuthLayout>
       <form>
-        <input placeholder="your name" />
-        <input placeholder="your password" />
-        <input />
+        <h1 className="mb-6 text-6xl font-bold text-center text-white">
+          Sign in
+        </h1>
+        <h1 className="mb-2 text-sm text-center text-white">
+          See what your friends are doing
+        </h1>
+
+        <input placeholder="your name" className=" border-t-black" />
+        <br></br>
+        <input placeholder="your password" className="mt-4 border-t-black" />
       </form>
     </AuthLayout>
   );
