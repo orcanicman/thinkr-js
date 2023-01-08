da403bdfcb82db41b381d1c5688370d3305adc37
Coyu97 <106992@student.drenthecollege.nl>
2023-01-08 23:49:07 +0100

    login darkmode layout

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -7,16 +7,37 @@ export const Login: React.FC<LoginProps> = () => {
   return (
     <AuthLayout>
       <form>
-        <h1 className="mb-6 text-6xl font-bold text-center text-white">
+        <h1 className="mb-8 text-6xl font-bold text-center text-white">
           Sign in
         </h1>
-        <h1 className="mb-2 text-sm text-center text-white">
-          See what your friends are doing
+        <h1 className="mb-6 font-light text-center text-white">
+          Sign in and see what your friends are thinking
         </h1>
 
-        <input placeholder="your name" className=" border-t-black" />
-        <br></br>
-        <input placeholder="your password" className="mt-4 border-t-black" />
+        <input
+          className="mb-8 placeholder:text-white placeholder:font-light bg-darkTransparentHighlight px-4 py-3 rounded-xl w-full"
+          placeholder="Email"
+        />
+
+        <input
+          className="mb-8 placeholder:text-white placeholder:font-light bg-darkTransparentHighlight px-4 py-3 rounded-xl w-full"
+          placeholder="Password"
+        />
+
+        <div className="flex items-center">
+          <input
+            type="checkbox"
+            className="mr-4 default:ring-darkTransparentHighlight accent-darkTransparentHighlight"
+          />
+          <div className="text-white font-light ">Remember me</div>
+
+          <div className="ml-32 text-green "> Forgot password?</div>
+        </div>
+        <div className="flex justify-center ">
+          <button className="mt-8  px-4 py-3 rounded-xl w-full  text-white font-light bg-green">
+            Login
+          </button>
+        </div>
       </form>
     </AuthLayout>
   );
